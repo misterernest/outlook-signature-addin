4ee8e0908ee541d277ea5d7859b81fdb1a06969a
misterernest <misterernest@hotmail.com>
2025-12-05 16:09:01 +1100

    add test of js 2

--- runtime.js
+++ runtime.js
@@ -1,12 +1,29 @@
-console.log("WWG runtime.js loaded");
+(function () {
+  console.log("WWG runtime.js loaded");
 
-function onNewMessageComposeHandler(event) {
-  console.log("WWG EVENT: OnNewMessageCompose fired");
+  function onNewMessageComposeHandler(event) {
+    console.log("WWG EVENT: OnNewMessageCompose fired");
 
-  event.completed();
-}
+    try {
+      // Mensaje visible en la barra amarilla/azul de Outlook
+      Office.context.mailbox.item.notificationMessages.add(
+        "wwgTest",
+        {
+          type: Office.MailboxEnums.ItemNotificationMessageType.InformationalMessage,
+          message: "WWG: OnNewMessageCompose se ejecutó.",
+          icon: "Icon.16x16", // opcional, definido en bt:Images
+          persistent: false
+        }
+      );
+    } catch (e) {
+      console.error("Error en handler:", e);
+    } finally {
+      event.completed();
+    }
+  }
 
-Office.actions.associate(
-  "onNewMessageComposeHandler",
-  onNewMessageComposeHandler
-);
+  Office.actions.associate(
+    "onNewMessageComposeHandler",
+    onNewMessageComposeHandler
+  );
+})();
